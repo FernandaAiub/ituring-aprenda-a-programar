234faaee3dedf3a0946887b033fe14734d73ba04
Fernanda Aiub <fernanda_aiub@hotmail.com>
2022-05-02 20:59:12 -0300

    Exercício 2

--- dia-3/exercício-2.js
+++ dia-3/exercício-2.js
@@ -4,30 +4,42 @@ let produtoC = { nome: 'sandália', valor: 120.0, internacional: false }
 
 if (produtoA.internacional == false) {
   let valorImpostos = produtoA.valor * 1.12
-  console.log('O produto é nacional')
-  console.log('As ' + produtoA.nome, 'tem um valor de ' + valorImpostos)
+  console.log(
+    'A ' + produtoA.nome,
+    'é nacional e tem um valor de ' + valorImpostos
+  )
 } else {
   let valorImpostos = produtoA.valor * 1.2
-  console.log('O produto é internacional')
-  console.log('As ' + produtoA.nome, 'tem um valor de ' + valorImpostos)
+  colnsole.log(
+    'A ' + produtoA.nome,
+    'é internacional e tem um valor de ' + valorImpostos
+  )
 }
 
 if (produtoB.internacional == false) {
   let valorImpostos = produtoB.valor * 1.12
-  console.log('O produto é nacional')
-  console.log('Os ' + produtoB.nome, 'tem um valor de ' + valorImpostos)
+  console.log(
+    'A ' + produtoB.nome,
+    'é nacional e tem um valor de ' + valorImpostos
+  )
 } else {
   let valorImpostos = produtoB.valor * 1.2
-  console.log('O produto é internacional')
-  console.log('Os ' + produtoB.nome, 'tem um valor de ' + valorImpostos)
+  colnsole.log(
+    'A ' + produtoB.nome,
+    'é internacional e tem um valor de ' + valorImpostos
+  )
 }
 
-if (produtoC.internacional == true) {
-  let valorImpostos = produtoC.valor * 1.2
-  console.log('O produto é internacional')
-  console.log('As ' + produtoC.nome, 'tem um valor de ' + valorImpostoss)
-} else {
+if (produtoC.internacional == false) {
   let valorImpostos = produtoC.valor * 1.12
-  console.log('O produto é nacional')
-  console.log('As ' + produtoC.nome, 'tem um valor de ' + valorImpostos)
+  console.log(
+    'A ' + produtoC.nome,
+    'é nacional e tem um valor de ' + valorImpostos
+  )
+} else {
+  let valorImpostos = produtoC.valor * 1.2
+  colnsole.log(
+    'A ' + produtoC.nome,
+    'é internacional e tem um valor de ' + valorImpostos
+  )
 }
